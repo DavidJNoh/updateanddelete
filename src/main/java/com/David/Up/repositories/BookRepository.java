f4e0de7ca0ca985fa6541285e3b8c4c974bf5330
David Noh <davidnohj@gmail.com>
2018-10-08 17:03:30 -0700

    wer

---
+++ src/main/java/com/David/Up/repositories/BookRepository.java
@@ -0,0 +1,20 @@
+package com.David.Up.repositories;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.David.Up.models.Book;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long>{
+	List<Book> findAll();
+//	List<Book> findByDescriptionContaining(String search);
+//	    // this method counts how many titles contain a certain string
+//	Long countByTitleContaining(String search);
+//	    // this method deletes a book that starts with a specific title
+//	Long deleteByTitleStartingWith(String search);
+}
